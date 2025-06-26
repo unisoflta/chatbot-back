72e57bb52a3cfc91a452ccf70776362b47c0da7e
unisoflta <admin@unisoftlta.com>
2025-06-26 11:18:18 -0500

    Switch from Sanctum to Passport for API

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -13,7 +13,7 @@ class BroadcastServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Broadcast::routes([
-            'middleware' => ['auth:sanctum']
+            'middleware' => ['auth:api']
         ]);
         require base_path('routes/channels.php');
     }
